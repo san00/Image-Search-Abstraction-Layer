dfc39139f410b53ea8522c443285f2980632ed38
Glitch (abstraktion) <none>
2018-03-23 19:34:51 +0000

    Connect custom search API

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // init project
 var express = require('express');
 var app = express();
-const api = process.env.APIKEY;
+const apiKey = process.env.GOOGLEAPIKEY;
 
 const mongoose = require('mongoose');
 const mongoClient = require('mongodb').MongoClient;
 const schema = require('./searchTerm');
 const db = process.env.MONGO;
+const GoogleImages = require('google-images');
+const client = new GoogleImages('cseId', 'apiKey');
+const cseId = process.env.CSEID;
+const gkey = process.env.GKEYTWO;
 
+
+var request = require('request');
+const https = require('https');
+const url = "https://www.googleapis.com/customsearch/v1" + '?key=' + gkey + '&cx=' + cseId + "&q=cats";
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,38 +28,65 @@ app.get("/", function (request, response) {
 
 mongoClient.connect(db, function(err,db){
 
-app.get("/api/search/:searchValue(*)", function (request, response) {
+
   
-  const searchTerm = request.params.searchValue;
+app.get("/api/search/", function (request, response, next) {
+  
+  const searchTerm = request.params.searchTerm 
   const offset = request.query;
+  let returnedData = "";
   
-  const data = new schema({
+  // node https module 
+ https.get(url, function (res){
+ // console.log(response);
+   console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+   
     
-    searchTerm,
-    searchDate: new Date()
-  });
+   res.on("data", function(data){
+     returnedData += data;
+           
+   });
+  res.on("end", function(){
+  console.log(returnedData);
+    
+    response.json(returnedData);
+    
+  })
+ }).on("error",function(e){
+ console.log(e);
+ }); 
   
-  data.save((err) => {
+}); 
+  
+  
+//   const data = new schema({
+//     searchTerm,
+//     searchDate: new Date()
+//   });
+  
+//   data.save((err) => {
 
-      if (err) {
-        console.log('ooops, sorry an error has occurred' + err);
-        response.send('error' + err)
-      }
-    });
-    return response.json(data);
+//       if (err) {
+//         console.log('ooops, sorry an error has occurred' + err);
+//         response.send('error' + err)
+//       }
+//     });
+//     return response.json(data);
 
-  });
+//   });
 
+  
 //Retrieve entire search history from database
- app.get("/api/searchItems", function(req, res, next){
+ app.get("/api/history", function(req, res, next){
    
     schema.find({}, function (err, data){
-         res.json(data);       
+        res.json(data);
     })
   });  
-});
   
- 
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
